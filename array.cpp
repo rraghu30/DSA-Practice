bd64ea7ac277b7028f783d79f7a1a31b549f9f02
rraghu30 <156595806+rraghu30@users.noreply.github.com>
2024-11-10 21:42:15 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
+ int basic[3]={1, 2, 3};
+
+    array<int,4> a ={1, 2, 3,4};
+
+    int size = a.size();
+
+    for(int i = 0; i<size; i++){
+        cout<<a[i]<<endl;
+
+    }
+
+    cout<<"Element at 2nd index"<<a.at(2)<<endl;
+
+    cout<<"empty or not"<<a.empty()<<endl;
 
     return 0;
 }
